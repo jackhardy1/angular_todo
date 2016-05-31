673bb7321261b6b2c4deb1651f76360dfea51ee4
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-31 16:40:37 +0100

    User can add and remove tasks

--- controllers.js
+++ controllers.js
@@ -1,5 +1,16 @@
 toDoApp.controller('ToDoController', function() {
-  this.todos = [
+
+  var self = this;
+
+  self.todos = [
     {task: 'Mow the lawn', completed: false},
     {task: 'Clean the toilet', completed: true}];
+
+  self.addToDo = function(taskName) {
+    self.todos.push({task: taskName, completed: false});
+  }
+
+  self.removeToDo = function(index) {
+    self.todos.splice(index, 1);
+  }
 });
